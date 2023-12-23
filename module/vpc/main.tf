afbe301ee92d05b7e3d396f3bb21af199e09ba2d
Balubadi <balubadi.pavankumar@gmail.com>
2023-12-23 15:21:17 +0530

    na

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -47,6 +47,11 @@ resource "aws_subnet" "db" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name="public"})
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
 resource "aws_route_table" "app" {
@@ -86,4 +91,10 @@ resource "aws_route_table_association" "db" {
   count          =length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.id
+}
+##create IGW and attach to public subnet
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id     = aws_vpc.main.id
+  tags       = merge(var.tags, {Name="igw"})
 }
