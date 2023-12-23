8126965e72736333807488ba0b005841540570e9
Balubadi <balubadi.pavankumar@gmail.com>
2023-12-23 13:02:18 +0530

    na

--- module/vpc/vars.tf
+++ module/vpc/vars.tf
@@ -1,4 +1,8 @@
  variable "vpc_cidr_block" {}
  variable "env" {}
-variable "tags" {}
-
+ variable "tags" {}
+ variable "public_subnets" {}
+ variable "web_subnets" {}
+ variable "app_subnets" {}
+ variable "db_subnets" {}
+ variable "azs" {}
