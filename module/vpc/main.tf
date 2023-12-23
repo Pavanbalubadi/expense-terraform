06aa7e730a8394e7d3ce1f04202f0f78986a784b
Balubadi <balubadi.pavankumar@gmail.com>
2023-12-23 15:49:57 +0530

    na

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -97,3 +97,11 @@ resource "aws_internet_gateway" "igw" {
   vpc_id     = aws_vpc.main.id
   tags       = merge(var.tags, {Name="igw"})
 }
+#create NGW in public subnet with one ip reserved(one elastic ip)
+resource "aws_eip" "ngw" {
+  domain   = "vpc"
+}
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public.*.id[0]
+}
