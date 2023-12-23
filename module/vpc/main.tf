691f6c27879b88397f529b702f9514a0aa28d0e8
Balubadi <balubadi.pavankumar@gmail.com>
2023-12-23 14:47:44 +0530

    na

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -9,3 +9,35 @@ resource "aws_subnet" "main" {
   tags       = merge(var.tags, {Name="public_subnets"})
   availability_zone = var.azs[count.index]
 }
+
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  tags       = merge(var.tags, {Name="public_subnets"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "web" {
+  count      = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  tags       = merge(var.tags, {Name="web_subnets"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "app" {
+  count      = length(var.app_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  tags       = merge(var.tags, {Name="app_subnets"})
+  availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "db" {
+  count      = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  tags       = merge(var.tags, {Name="db_subnets"})
+  availability_zone = var.azs[count.index]
+}
