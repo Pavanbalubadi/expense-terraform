f0d5fbc93565825171e0f81d99e652689fb3c4b8
Balubadi <balubadi.pavankumar@gmail.com>
2023-12-30 22:14:10 +0530

    peering connection

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source                  = "./module/vpc"
-  vpc_cidr_block          = var.vpc_cidr_block
-  env                     = var.env
-  tags                    = var.tags
-  public_subnets          = var.public_subnets
-  web_subnets             = var.web_subnets
-  app_subnets             = var.app_subnets
-  db_subnets              = var.db_subnets
-  azs                     = var.azs
-  account_id              = var.account_id
-  default_vpc_id          = var.default_vpc_id
-  default_route_table_id  = var.default_route_table_id
-  default_vpc_cidr_id     =var.default_vpc_cidr_id
+  source                 = "./module/vpc"
+  vpc_cidr_block         = var.vpc_cidr_block
+  env                    = var.env
+  tags                   = var.tags
+  public_subnets         = var.public_subnets
+  web_subnets            = var.web_subnets
+  app_subnets            = var.app_subnets
+  db_subnets             = var.db_subnets
+  azs                    = var.azs
+  account_id             = var.account_id
+  default_vpc_id         = var.default_vpc_id
+  default_route_table_id = var.default_route_table_id
+  default_vpc_cidr_id    = var.default_vpc_cidr_id
 }
 
 module "rds" {
--- vars.tf
+++ vars.tf
@@ -15,6 +15,3 @@ variable "rds_allocated_storage" {}
 variable "rds_engine" {}
 variable "rds_engine_version" {}
 variable "rds_instance_class" {}
-variable "subnets" {}
-variable "vpc_id" {}
-variable "sg_cidrs" {}
