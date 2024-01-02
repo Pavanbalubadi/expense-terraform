152d0f29de43018acae9bd3c4e8b8c1ed2a91eaa
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-02 21:33:27 +0530

    backend connection

--- module/app/userdata.sh
+++ module/app/userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 yum install ansible -y | tee -a /opt/userdata.logs
-ansible-pull -i localhost -U https://github.com/Pavanbalubadi/expense-ansible-1 expense.yml role_name=${role_name} env=${env}
+ansible-pull -i localhost, -U https://github.com/Pavanbalubadi/expense-ansible-1 expense.yml -e role_name=${role_name} -e env=${env}
