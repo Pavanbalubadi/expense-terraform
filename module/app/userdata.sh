d3b1ab5f34e0cacb4c7559036b94a6183089ba52
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-02 21:37:37 +0530

    backend connection

--- module/app/userdata.sh
+++ module/app/userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 yum install ansible  python3.11-pip.norch -y | tee -a /opt/userdata.logs
+pip3.11 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/Pavanbalubadi/expense-ansible-1 expense.yml -e role_name=${role_name} -e env=${env}
