89c3034d1aaab513aa98ebdc1e4811d466ee7fbb
Balubadi <balubadi.pavankumar@gmail.com>
2024-01-02 21:35:59 +0530

    backend connection

--- module/app/userdata.sh
+++ module/app/userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install ansible -y | tee -a /opt/userdata.logs
+yum install ansible  python3.11-pip.norch -y | tee -a /opt/userdata.logs
 ansible-pull -i localhost, -U https://github.com/Pavanbalubadi/expense-ansible-1 expense.yml -e role_name=${role_name} -e env=${env}
