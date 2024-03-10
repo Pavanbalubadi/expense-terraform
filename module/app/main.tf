4a2a52dd8203931755a08245baec102d00a6bead
Balubadi <balubadi.pavankumar@gmail.com>
2024-03-10 20:17:55 +0530

    making shell scripting  ading underline addling topics sample fine

--- module/app/main.tf
+++ module/app/main.tf
@@ -60,3 +60,31 @@ resource "aws_autoscaling_group" "main" {
     }
 
 }
+
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.component}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+    })
+  }
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
