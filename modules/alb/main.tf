64787cfe4ba9ce2cd1b89684b2dca3967fd72920
pavan <balubadipavan@gmail.com>
2024-03-31 11:31:12 +0530

    expense terraform alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,18 +1,9 @@
 resource "aws_lb" "test" {
-  name               = "test-lb-tf"
-  internal           = false
+  name               = "${var.env}-${var.component}-alb"
+  internal           = var.internal
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.id
-    prefix  = "test-lb"
-    enabled = true
-  }
-
+  subnets            = var.subnets
   tags = {
     Environment = "production"
   }
