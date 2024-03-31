f5c99cb11274f87ce6b00ccf99d2b5d71d9c9d8c
pavan <balubadipavan@gmail.com>
2024-03-31 11:36:57 +0530

    expense terraform alb

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,10 +1,41 @@
+resource "aws_security_group" "main" {
+  name        = "${var.env}-${var.component}"
+  description = "${var.env}-${var.component}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "APP"
+    from_port   = var.lb_port
+    to_port     = var.lb_port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_cidrs
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags       = merge(var.tags, { Name = "${var.env}-${var.component}-alb" })
+}
 resource "aws_lb" "test" {
   name               = "${var.env}-${var.component}-alb"
   internal           = var.internal
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-  tags = {
-    Environment = "production"
-  }
+  tags       = merge(var.tags, { Name = "${var.env}-${var.component}-alb" })
 }
+
+variable "env" {}
+variable "component" {}
+variable "vpc_id" {}
+variable "lb_port" {}
+variable "sg_cidrs" {}
+variable "tags" {}
+variable "internal" {}
+variable "subnets" {}
