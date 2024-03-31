16829d51e4c487fbd6099ba8ced42d109b40cbb4
pavan <balubadipavan@gmail.com>
2024-03-31 12:50:02 +0530

    expense terraform kms

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -32,6 +32,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_listener" "main" {
+  count             = var.enable_https ? 0 : 1 # terraform conditions
   load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
@@ -43,6 +44,7 @@ resource "aws_lb_listener" "main" {
 }
 
 resource "aws_lb_listener" "https" {
+  count             = var.enable_https ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
@@ -51,6 +53,23 @@ resource "aws_lb_listener" "https" {
   default_action {
     type             = "forward"
     target_group_arn = var.target_group_arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  count             = var.enable_https ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
